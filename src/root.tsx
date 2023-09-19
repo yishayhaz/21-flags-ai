964955175a80b4d9588f5ab0758f3c7d6bac4ccc
Yishay Hazan <yishay11013@gmail.com>
2023-09-19 17:31:06 +0300

    perfect.

--- src/root.tsx
+++ src/root.tsx
@@ -22,6 +22,11 @@ export default component$(() => {
         <link rel="manifest" href="/manifest.json" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>21 Flags</title>
+        <meta
+          name="description"
+          content="On your turn, you can remove 1, 2 or 3 flags.
+The player who removes the last flag wins."
+        />
         <link
           rel="icon"
           href="https://cdn1.iconfinder.com/data/icons/cute-color-robot/32/cute_robot_emoji_evil_avatar_machine_emoticon-512.png"
